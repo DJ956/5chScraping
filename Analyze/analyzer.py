338565835fa3ffa7e221eff7c34fe4db77a4c964
DJ956 <dexter13576@gmail.com>
2019-03-15 19:30:58 +0900

    add most word list
    最頻出ワードリストを出力する機能を実装。出力結果は指定した個数の最頻出単語リストとそのグラフ

--- Analyze/analyzer.py
+++ Analyze/analyzer.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
-#1:モデル(*.model)
-#2:類義語(文字列)
-#-*-coding:utf-8-*-
 #1:data.txt
+#2:最頻出単語表示数
+
 import MeCab
 import re
 import sys
-import time
+import matplotlib.pyplot as plt
 
 #名詞の中の一般と固有名詞だけをlistで返す
 def filter(documents):
@@ -24,6 +23,43 @@ def filter(documents):
 
 	return word_list
 
+#単語の出現回数をカウント
+def count(word_list, show_count):
+	dicts = {}
+	for count in word_list:
+		if count not in dicts:
+			dicts.setdefault(count, 1)
+		else:
+			dicts[count] += 1
+
+	total = sum(dicts.values())	
+	print("総単語数:{}".format(total))
+
+	sorted_dict = {}
+	index = 0
+	for k, v in sorted(dicts.items(), key=lambda x: x[1], reverse=True):
+		sorted_dict.update({str(k):int(v)})
+		index += 1
+		if index > show_count:
+			break
+
+	return sorted_dict, total
+
+#結果をグラフにして出力する
+def create_plt(dicts, show_count, total):
+	plt.figure(figsize=(15, 5))
+	plt.title(u'最頻出ワードベスト:{0} 総単語数:{1}'.format(show_count, total), size=16)
+	plt.bar(range(len(dicts)), list(dicts.values()), align='center')
+	plt.xticks(range(len(dicts)), list(dicts.keys()))
+
+	for x, y in zip(range(len(dicts)), dicts.values()):
+		plt.text(x, y, y, ha='center', va='bottom')
+		plt.text(x, y/2, u'{0}%'.format(round((y/total*100), 3)), ha='center', va='bottom')
+	plt.tick_params(width=2, length=10)
+	plt.tight_layout()
+
+	plt.savefig("result.png")
+
 #テキストデータをロードして文字列を返す
 def load(path):	
 	doc = []
@@ -36,13 +72,38 @@ def load(path):
 
 	return ''.join(doc)
 
+def save(path, dicts, show_count):
+	index = 0
+	with open(path, 'w', encoding='utf-8') as fo:
+		for k, v in dicts.items():
+			fo.write("{0}/{1} {2}:{3}回\n".format(index, show_count, k, v))
+			fo.write("-" * 30)
+			fo.write('\n')
+			index += 1
+
 def main():
 	argv = sys.argv
 
-	doc = load(argv[1])	
+	load_path = argv[1]
+	out_path  = argv[2]
+	show_count = int(argv[3])
+
+	print("テキストデータロード開始...")
+	doc = load(load_path)	
+	print("テキストデータロード完了")
+
+	print("固有名詞のみを抽出中...")
 	word_list = filter(doc)
+	print("抽出完了")
+
+	print("単語の出現回数をカウント中...")
+	dicts, total = count(word_list, show_count)
+
+	print("結果を出力中...")
+	create_plt(dicts, show_count, total)
+	save(out_path, dicts, show_count)
+	print("出力完了:{}".format(out_path))
 	
-	print(word_list)
 
 if __name__ == '__main__':
 	main()
