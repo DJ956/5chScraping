437496056923edb33704636b81a06cc4b7e747e4
DJ956 <dexter13576@gmail.com>
2019-03-15 18:18:31 +0900

    Add word count
    頻出頻度が高い単語をリストアップするAnalyze.pyを実装中。現段階ではdata.txtから名詞のみを抽出する機能のみを実装

--- Analyze/analyzer.py
+++ Analyze/analyzer.py
@@ -1,17 +1,48 @@
 # -*- coding: utf-8 -*-
 #1:モデル(*.model)
 #2:類義語(文字列)
-
-from gensim.models import word2vec
+#-*-coding:utf-8-*-
+#1:data.txt
+import MeCab
+import re
 import sys
+import time
+
+#名詞の中の一般と固有名詞だけをlistで返す
+def filter(documents):
+	tagger = MeCab.Tagger()
+	wakati = tagger.parse(documents).split('\n')
+	
+	word_list = []
+	for addlist in wakati:
+		addlist = re.split('[\t,]', addlist)		
+		if addlist == [] or addlist[0] == 'EOS' or addlist[0] == '' or addlist[0] == 'ー' or addlist[0] == '*':
+			pass
+		elif addlist[1] == '名詞':
+			if addlist[2] == '一般' or addlist[2] == '固有名詞':
+				word_list.append(addlist[0])
+
+	return word_list
+
+#テキストデータをロードして文字列を返す
+def load(path):	
+	doc = []
+	with open(path, 'r', encoding = 'utf-8') as f:		
+		line = f.readline()
+		while line:
+			doc.append(line)
+			line = f.readline()
+		print("総文字数:{}".format(len(doc)))
+
+	return ''.join(doc)
 
 def main():
 	argv = sys.argv
 
-	model = word2vec.Word2Vec.load(argv[1])
-	results = model.most_similar(positive=[argv[2]])
-	for result in results:
-		print(result)
+	doc = load(argv[1])	
+	word_list = filter(doc)
+	
+	print(word_list)
 
 if __name__ == '__main__':
 	main()
